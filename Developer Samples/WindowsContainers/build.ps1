e6ad1c951c3c0ee932af207f78b7f1cfe19655d6
Josh Elster <jelster@inrule.com>
2018-09-12 15:19:33 -0500

    add support for pulling source artifacts from AppServices GH repos.
    Hack: Disable WCF bindings for SSL in DOCKERFILEs

--- Developer Samples/WindowsContainers/build.ps1
+++ Developer Samples/WindowsContainers/build.ps1
@@ -1,64 +1,57 @@
 [CmdletBinding(SupportsShouldProcess=$true)]
 param(
-    [string]$tag,
+    [string][Parameter(Mandatory= $true)] $tag,
     [string]$registryRootRepos = "inrule",
     [switch]$setLatestTag = $false,
-    [switch]$skipServerBuild = $false,
-    [string]$defaultInRuleInstallFolder = "C:\Program Files (x86)\InRule\"
+    [switch]$skipServerBuild = $false
 )
-Push-Location $PSScriptRoot
-
-$version = $null
-Write-Host "Attempting to determine the version number for assets located at $defaultInRuleInstallFolder"
-$version = (Get-ChildItem $defaultInRuleInstallFolder\*\InRule.Common.dll -recurse)[0].VersionInfo.ProductVersion
-Write-Host "Using InRule version $version"
-
-if (([String]::IsNullOrWhiteSpace($tag) -and ($null -ne $version))) {
-    Write-Host "No value supplied for image tag. Using $version for tagging images."
-    $tag = $version
-}
+$ErrorActionPreference = "Stop"
 Write-Host "Images will be tagged $tag. Also tag them as latest? $setLatestTag"
 
 $ErrorActionPreference = "Stop"
 if ($skipServerBuild -eq $false) { 
-    write-host "Building inrule-server base image."
-    set-location "$PSScriptRoot\inrule-server" 
+    write-host "Building inrule-server base image."    
     if ($PSCmdlet.ShouldProcess("Building inrule-server base image")) {
-        docker build --label "com.inrule.version=$version" -t ${registryRootRepos}/inrule-server:$tag . 
+        docker build --label "com.inrule.version=$tag" -t ${registryRootRepos}/inrule-server:$tag  -f "$PWD\inrule-server\DOCKERFILE" "$PWD\inrule-server"
+
+        if ($LASTEXITCODE -ne 0) {
+            throw 'non-zero return from docker build. aborting.'
+            
+        }
     }
-    if ($setLatestTag -eq $true -and $PSCmdlet.ShouldProcess("Setting image $version to latest...")) {
+    if ($setLatestTag -eq $true -and $PSCmdlet.ShouldProcess("Setting image $tag to latest...")) {
         docker image tag ${registryRootRepos}/inrule-server:$tag ${registryRootRepos}/inrule-server:latest    
     }
 }
 
-$hostCatalogPath = resolve-path "$defaultInRuleInstallFolder\irServer\RepositoryService\IisService\"
-$catalogBuildPath = "$PSScriptRoot\inrule-catalog"
-set-location $catalogBuildPath
-if ($PSCmdlet.ShouldProcess("Building inrule-catalog image using assets at $hostCatalogPath")) {
-    Remove-Item -Path $catalogBuildPath\irCatalog\ -Include *.* -Recurse -Force
-    Copy-Item $hostCatalogPath\** -Recurse -Force -Destination "$catalogBuildPath\irCatalog\" 
-    docker build --label "com.inrule.version=$version" -t ${registryRootRepos}/inrule-catalog:$tag .
+if ($PSCmdlet.ShouldProcess("Building inrule-catalog image")) {
+   
+    docker build --label "com.inrule.version=$tag" -t ${registryRootRepos}/inrule-catalog:$tag -f $PWD\inrule-catalog\DOCKERFILE $PWD\inrule-catalog
+
+    if ($LASTEXITCODE -ne 0) {
+        throw 'non-zero return from docker build. aborting.'
+        
+    }
 }
 
-$hostRuntimePath = resolve-path $defaultInRuleInstallFolder\irServer\RuleEngineService\IisService
-$runtimeBuildPath = "$PSScriptRoot\inrule-runtime"
-set-location $runtimeBuildPath
-if ($PSCmdlet.ShouldProcess("Building inrule-runtime image using assets at $hostRuntimePath")) {
-    Remove-Item -Path $runtimeBuildPath\irServer\ -Include *.* -Recurse -Force
-    Copy-Item -Path $hostRuntimePath\** -Recurse -Force -Destination "$runtimeBuildPath\irServer"
-    docker build --label "com.inrule.version=$version" -t ${registryRootRepos}/inrule-runtime:$tag .
+if ($PSCmdlet.ShouldProcess("Building inrule-runtime image")) {   
+    docker build --label "com.inrule.version=$tag" -t ${registryRootRepos}/inrule-runtime:$tag -f $PWD\inrule-runtime\DOCKERFILE $PWD\inrule-runtime
+
+    if ($LASTEXITCODE -ne 0) {
+        throw 'non-zero return from docker build. aborting.'
+        
+    }
 }
 
-$hostCatManPath = resolve-path $defaultInRuleInstallFolder\irServer\CatalogManagerWeb
-$catmanBuildPath = "$PSScriptRoot\inrule-catalog-manager"
-set-location $catmanBuildPath
-if ($PSCmdlet.ShouldProcess("Building inrule-catalog-manager image using assets at $hostCatManPath")) {
-    Remove-Item -Path $catmanBuildPath\CatalogManagerWeb -Include *.* -Recurse -Force
-    Copy-Item -Path $hostCatManPath\** -Recurse -Force -Destination "$catmanBuildPath\CatalogManagerWeb"
-    docker build --label "com.inrule.version=$version" -t ${registryRootRepos}/inrule-catalog-manager:$tag .
+if ($PSCmdlet.ShouldProcess("Building inrule-catalog-manager image")) {    
+    docker build --label "com.inrule.version=$tag" -t ${registryRootRepos}/inrule-catalog-manager:$tag -f $PWD\inrule-catalog-manager\DOCKERFILE $PWD\inrule-catalog-manager
+    if ($LASTEXITCODE -ne 0) {
+        throw 'non-zero return from docker build. aborting.'
+        
+    }
 }
 
-if ($setLatestTag -eq $true -and $PSCmdlet.ShouldProcess("Setting image $tag to latest...") ) {
+if ($setLatestTag -and $PSCmdlet.ShouldProcess("Setting image $tag to latest...") ) {
     docker image tag ${registryRootRepos}/inrule-catalog:$tag ${registryRootRepos}/inrule-catalog:latest
 
     docker image tag ${registryRootRepos}/inrule-runtime:$tag ${registryRootRepos}/inrule-runtime:latest
