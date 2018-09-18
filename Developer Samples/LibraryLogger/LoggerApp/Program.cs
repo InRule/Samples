b7227e2ff25675aac2f594f3a73dce9b5c3bb09b
Lucas Konopka <LKonopka@inrule.com>
2018-09-18 14:28:34 -0500

    Add LibraryLogger sample application
    SEAL-1297.

---
+++ Developer Samples/LibraryLogger/LoggerApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InRule.Repository;
+using InRule.Runtime;
+using Serilog;
+
+namespace LibraryLoggerSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            Log.Logger = log;
+
+            var ruleAppDef = new RuleApplicationDef();
+            var entity1Def = ruleAppDef.Entities.Add(new EntityDef("entity1"));
+            var fieldDef = new FieldDef();
+            
+
+            fieldDef.IsCalculated = true;
+            fieldDef.Calc.FormulaText = "1 + 1";
+
+            entity1Def.Fields.Add(fieldDef);
+
+            using (var session = new RuleSession(ruleAppDef))
+            {
+                var entity1 = session.CreateEntity(entity1Def.Name);
+                session.ApplyRules();
+                Console.WriteLine(entity1.Fields[fieldDef.Name].Value.ToString());
+            }
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Developer Samples/LibraryLogger/README.md
@@ -0,0 +1,11 @@
+# InRule Samples
+
+## LibraryLogger
+
+This sample shows how you can use irSDK get InRule Application Logs using Serilog.
+
+This application does the following
+* Add the LibraryLogger to your config file
+* Creates a Serilog Console Logger
+* Displays logs from a RuleSession in the console
+
