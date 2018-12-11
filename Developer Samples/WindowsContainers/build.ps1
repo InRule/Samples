bffcaf62673b4043ca93c810bc76c8e6f3ae58d8
Josh Elster <jelster@inrule.com>
2018-12-11 17:16:25 -0600

    Add file output of built images and tags

--- Developer Samples/WindowsContainers/build.ps1
+++ Developer Samples/WindowsContainers/build.ps1
@@ -8,30 +8,33 @@ param(
 $ErrorActionPreference = "Stop"
 Write-Host "Images will be tagged $tag. Also tag them as latest? $setLatestTag"
 
-$ErrorActionPreference = "Stop"
+$imagesBuild = @()
+
 if ($skipServerBuild -eq $false) { 
     write-host "Building inrule-server base image."    
     if ($PSCmdlet.ShouldProcess("Building inrule-server base image")) {
         docker build --label "com.inrule.version=$tag" -t ${registryRootRepos}/inrule-server:$tag  -f "$PWD\inrule-server\DOCKERFILE" "$PWD\inrule-server"
-
+        
         if ($LASTEXITCODE -ne 0) {
             throw 'non-zero return from docker build. aborting.'
             
         }
-    }
-    if ($setLatestTag -eq $true -and $PSCmdlet.ShouldProcess("Setting image $tag to latest...")) {
-        docker image tag ${registryRootRepos}/inrule-server:$tag ${registryRootRepos}/inrule-server:latest    
-    }
+        $imagesBuild += "${registryRootRepos}/inrule-server:${tag}"
+        if ($setLatestTag -eq $true) {
+            docker image tag ${registryRootRepos}/inrule-server:$tag ${registryRootRepos}/inrule-server:latest    
+        }
+        $imagesBuild += "${registryRootRepos}/inrule-server:latest"
+    }    
 }
 
 if ($PSCmdlet.ShouldProcess("Building inrule-catalog image")) {
    
     docker build --build-arg reposTag=$tag --label "com.inrule.version=$tag" -t ${registryRootRepos}/inrule-catalog:$tag -f $PWD\inrule-catalog\DOCKERFILE $PWD\inrule-catalog
-
+    
     if ($LASTEXITCODE -ne 0) {
         throw 'non-zero return from docker build. aborting.'
-        
     }
+    $imagesBuild += "${registryRootRepos}/inrule-catalog:${tag}"
 }
 
 if ($PSCmdlet.ShouldProcess("Building inrule-runtime image")) {   
@@ -39,24 +42,29 @@ if ($PSCmdlet.ShouldProcess("Building inrule-runtime image")) {
 
     if ($LASTEXITCODE -ne 0) {
         throw 'non-zero return from docker build. aborting.'
-        
     }
+    $imagesBuild += "${registryRootRepos}/inrule-runtime:${tag}"
 }
 
 if ($PSCmdlet.ShouldProcess("Building inrule-catalog-manager image")) {    
     docker build --build-arg reposTag=$tag --label "com.inrule.version=$tag" -t ${registryRootRepos}/inrule-catalog-manager:$tag -f $PWD\inrule-catalog-manager\DOCKERFILE $PWD\inrule-catalog-manager
     if ($LASTEXITCODE -ne 0) {
         throw 'non-zero return from docker build. aborting.'
-        
     }
+    $imagesBuild += "${registryRootRepos}/inrule-catalog-manager:${tag}"
 }
 
 if ($setLatestTag -and $PSCmdlet.ShouldProcess("Setting image $tag to latest...") ) {
     docker image tag ${registryRootRepos}/inrule-catalog:$tag ${registryRootRepos}/inrule-catalog:latest
-
+    $imagesBuild += "${registryRootRepos}/inrule-catalog:latest"
     docker image tag ${registryRootRepos}/inrule-runtime:$tag ${registryRootRepos}/inrule-runtime:latest
-
+    $imagesBuild += "${registryRootRepos}/inrule-runtime:latest"
     docker image tag ${registryRootRepos}/inrule-catalog-manager:$tag ${registryRootRepos}/inrule-catalog-manager:latest
+    $imagesBuild += "${registryRootRepos}/inrule-catalog-manager:latest"
 }
+if ($PSCmdlet.ShouldProcess("Write file with images and tags")) {
+    $imagesBuild | set-content -Path "$PWD\docker-build" -Force
+}
+
 Pop-Location
 
