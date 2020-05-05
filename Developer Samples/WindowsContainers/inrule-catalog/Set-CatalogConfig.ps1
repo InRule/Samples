65f3237589c9a84a9ff941320c531af426a7734c
Josh Elster <jelster@inrule.com>
2020-05-05 13:15:48 -0500

    * Add log/service monitor to base server.
    * integrate log monitoring w/ catalog service.
    
    * Add runtime SSL binding support for catalog
    
    * Add support for mounting and using PFX.
    
    * Mount folder containing PFX into a subfolder of $env:ContainerDir
    
    * specify PFX PW with --env PfxPassword=<password> for docker run

--- Developer Samples/WindowsContainers/inrule-catalog/Set-CatalogConfig.ps1
+++ Developer Samples/WindowsContainers/inrule-catalog/Set-CatalogConfig.ps1
@@ -3,7 +3,8 @@
 param(
     [string]$connectionString = $env:inrule:repository:service:connectionString,
     [string]$installPath = "$env:ContainerDir",
-    [string]$logLevel = "$env:irLogLevel"    
+    [string]$logLevel = "$env:irLogLevel",
+    [string]$wcfCfgFilePath = "$env:ContainerDir\\configfragments.xml"    
 )
 function Set-CatalogConfig {    
     function New-AppSettingsEntry {
@@ -13,6 +14,56 @@ function Set-CatalogConfig {
         $appSetting.SetAttribute("key", $key)
         $appSetting.SetAttribute("value", $value)
         return $appSetting
+    }
+
+    function Configure-SslCert {    
+        $pwSecString = ConvertTo-SecureString -String $env:PfxPassword -AsPlainText -Force
+        $certPath = (gci $installPath\*.pfx -recurse)[0]
+        $cert = Import-PfxCertificate -FilePath $certPath.FullName -Password $pwSecString -CertStoreLocation cert:\LocalMachine\My
+        $certHashString = $cert.GetCertHashString()
+        Write-Verbose "Imported PFX $certHashString to 'cert:\LocalMachine\My'"
+        (Get-WebBinding -Protocol "https").AddSslCertificate($certHashString, "My")
+        Write-Verbose "Added SSL certificate with thumbprint $certHashString to HTTPS binding"
+    }
+
+    function Replace-WcfBindings {
+        param([XML]$cfgXml)
+        $newSvcModelCfg = [xml]$(get-content $wcfCfgFilePath)
+        $cfgXml.configuration.RemoveChild($cfgXml.configuration.'system.serviceModel')
+        $newNode = $cfgXml.ImportNode($newSvcModelCfg.'system.serviceModel', $true)
+        $cfgXml.configuration.AppendChild($newNode)
+        
+    }
+    function Install-NuGetAndPackages {
+        param([XML]$cfgXml)
+        Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile nuget.exe;
+        .\nuget install $installPath\packages.config -OutputDirectory $installPath\bin\
+
+       
+        $cfgXml.configuration.appSettings.SetAttribute("configBuilders", "EnvironmentVariables, KeyPerFile")
+        $cfgBuilderSection = $cfgXml.CreateElement("section")
+        $cfgBuilderSection.SetAttribute("name", "configBuilders")
+        $cfgBuilderSection.SetAttribute("type", "System.Configuration.ConfigurationBuildersSection, System.Configuration, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
+        $cfgXml.configuration.configSections.appendChild($cfgBuilderSection)
+
+        $cfgBuilder = $cfgXml.CreateElement("configBuilders")
+        $cfgXml.configuration.appendChild($cfgBuilder)
+        
+        $builders = $cfgXml.CreateElement("builders")
+        $cfgBuilder.appendChild($builders)
+
+        $envBuilder = $cfgXml.CreateElement("add")
+        $envBuilder.SetAttribute("name", "EnvironmentVariables")
+        $envBuilder.SetAttribute("mode", "Greedy")
+        $envBuilder.SetAttribute("type", "Microsoft.Configuration.ConfigurationBuilders.EnvironmentConfigBuilder, Microsoft.Configuration.ConfigurationBuilders.Environment, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")
+        $builders.appendChild($envBuilder)
+
+        $kpfBuilder = $cfgXml.CreateElement("add")
+        $kpfBuilder.SetAttribute("name", "KeyPerFile")
+        $kpfBuilder.SetAttribute("mode", "Greedy")
+        $kpfBuilder.SetAttribute("type", "Microsoft.Configuration.ConfigurationBuilders.KeyPerFileConfigBuilder, Microsoft.Configuration.ConfigurationBuilders.KeyPerFile, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35"
+)
+        $builders.appendChild($kpfBuilder)
     }
 
     write-output "Replacing supplied configuration values in web.config..."
@@ -25,6 +76,9 @@ function Set-CatalogConfig {
 
     $cfgXml = new-object Xml
     $cfgXml.Load($configFilePath)
+    $cfgXml.configuration.'system.web'.compilation.setAttribute("targetFramework", "4.7.2")
+    $cfgXml.configuration.'system.web'.httpRuntime.setAttribute("targetFramework", "4.7.2")
+
     $catSvc = $cfgXml.configuration["appSettings"]
     if ($null -eq $catSvc) {
          
@@ -32,6 +86,14 @@ function Set-CatalogConfig {
         $cfgXml.configuration.appendChild($catSvc)
         write-verbose "Added appSettings node to 'inrule.repository.service'"
     }
+    
+    #Install-NugetAndPackages $cfgXml
+    if ((gci $installPath\*.pfx -recurse).Count -gt 0) {
+        Configure-SslCert
+    }
+    #
+    Replace-WcfBindings $cfgXml
+    
 
     $cs = New-AppSettingsEntry -parentDoc $cfgXml -key "inrule:repository:service:connectionString" -value $connectionString
     $logLevel = New-AppSettingsEntry -parentDoc $cfgXml -key 'inrule:logging:level' -value $logLevel
@@ -40,8 +102,7 @@ function Set-CatalogConfig {
     $catSvc.AppendChild($logLevel)
    
 
-    $inruleLogSection = $cfgXml.configuration.'inrule.logging'
-    
+    $inruleLogSection = $cfgXml.configuration.'inrule.logging'    
     $inruleLogSection.group.logger.option.SetAttribute("value", "InRule") # ensure logs are sourced to the InRule eventSource
 
     $cfgXml.Save($configFilePath)
