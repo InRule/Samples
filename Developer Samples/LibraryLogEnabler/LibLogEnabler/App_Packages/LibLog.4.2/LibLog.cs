81f16e6b067a2367e3a8ebe2aa44ce005c4ff607
Eugene Zektser <ezektser@inrule.com>
2020-05-19 15:16:17 -0500

    nuget updates

--- Developer Samples/LibraryLogEnabler/LibLogEnabler/App_Packages/LibLog.4.2/LibLog.cs
+++ Developer Samples/LibraryLogEnabler/LibLogEnabler/App_Packages/LibLog.4.2/LibLog.cs
@@ -40,23 +40,23 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "TraceLogger.Logging")]
-[assembly: SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Scope = "member", Target = "TraceLogger.Logging.Logger.#Invoke(TraceLogger.Logging.LogLevel,System.Func`1<System.String>,System.Exception,System.Object[])")]
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "LibLogEnabler.Logging")]
+[assembly: SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Scope = "member", Target = "LibLogEnabler.Logging.Logger.#Invoke(LibLogEnabler.Logging.LogLevel,System.Func`1<System.String>,System.Exception,System.Object[])")]
 
 // If you copied this file manually, you need to change all "YourRootNameSpace" so not to clash with other libraries
 // that use LibLog
 #if LIBLOG_PROVIDERS_ONLY
-namespace TraceLogger.LibLog
+namespace LibLogEnabler.LibLog
 #else
-namespace TraceLogger.Logging
+namespace LibLogEnabler.Logging
 #endif
 {
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
 #if LIBLOG_PROVIDERS_ONLY
-    using TraceLogger.LibLog.LogProviders;
+    using LibLogEnabler.LibLog.LogProviders;
 #else
-    using TraceLogger.Logging.LogProviders;
+    using LibLogEnabler.Logging.LogProviders;
 #endif
     using System;
 #if !LIBLOG_PROVIDERS_ONLY
@@ -714,9 +714,9 @@ namespace TraceLogger.Logging
 }
 
 #if LIBLOG_PROVIDERS_ONLY
-namespace TraceLogger.LibLog.LogProviders
+namespace LibLogEnabler.LibLog.LogProviders
 #else
-namespace TraceLogger.Logging.LogProviders
+namespace LibLogEnabler.Logging.LogProviders
 #endif
 {
     using System;
