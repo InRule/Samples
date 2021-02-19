1b1b04278e7c26673f16aa1285f0d220548cd20f
Mircea MATEI <matei_tm@yahoo.com>
2021-02-19 11:19:25 +0200

    Adding a data driven MsTest wrapper project for TestSuite

---
+++ Developer Samples/CommandLineTools/MstestWrapperForTestsuite/DataDrivenWrapperForTestsuite.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace MstestWrapperForTestsuite
+{
+    [TestClass]
+    public class DataDrivenWrapperForTestsuite : DataDrivenWrapperForTestsuiteBase
+    {
+        [TestMethod]
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\data\\testsuitelist.csv",
+            "testsuitelist#csv",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("data\\testsuitelist.csv")]
+        public void TestInRuleTestSuite()
+        {
+            var ruleAppFilePath = TestContext.DataRow["RuleaAppFilePath"].ToString();
+            var testSuiteFilePath = TestContext.DataRow["TestSuiteFilePath"].ToString();
+
+            var testResultCollection = RunTestSuite(ruleAppFilePath, testSuiteFilePath);
+
+            TestContext.WriteLine("Using Rule App " + ruleAppFilePath);
+            TestContext.WriteLine("Using Test Suite " + testSuiteFilePath);
+
+            foreach (var result in testResultCollection)
+            {
+                if (result.RuntimeErrorMessage != null)
+                {
+                    TestContext.WriteLine($"ERROR: Failed to execute test {result.TestDef.DisplayName}: {result.RuntimeErrorMessage}");
+                }
+                else if (result.Passed)
+                {
+                    TestContext.WriteLine($"PASS: {result.TestDef.DisplayName}");
+                }
+                else
+                {
+                    TestContext.WriteLine($"FAIL: {result.TestDef.DisplayName}");
+                    foreach (var failedAssertionResult in result.AssertionResults.Where(ar => ar.Passed == false))
+                    {
+                        TestContext.WriteLine($"  {failedAssertionResult.Target} was {failedAssertionResult.ActualValue}, expected value {failedAssertionResult.ExpectedValue}");
+                    }
+                }
+
+                Assert.AreEqual(true, result.Passed);
+                Assert.IsNull(result.RuntimeErrorMessage);
+            }
+        }
+    }
+}
---
+++ Developer Samples/CommandLineTools/MstestWrapperForTestsuite/DataDrivenWrapperForTestsuiteBase.cs
@@ -0,0 +1,44 @@
+﻿using InRule.Repository;
+using InRule.Repository.Regression;
+using InRule.Runtime;
+using InRule.Runtime.Testing.Regression;
+using InRule.Runtime.Testing.Regression.Runtime;
+using InRule.Runtime.Testing.Session;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace MstestWrapperForTestsuite
+{
+    [TestClass]
+    public abstract class DataDrivenWrapperForTestsuiteBase
+    {
+        public TestContext TestContext { get; set; }
+
+        protected static TestResultCollection RunTestSuite(string ruleAppFilePath, string testSuiteFilePath)
+        {
+            if (!File.Exists(ruleAppFilePath))
+            {
+                throw new FileNotFoundException("ERROR: Rule App file not found at " + ruleAppFilePath);
+            }
+            if (!File.Exists(testSuiteFilePath))
+            {
+                throw new FileNotFoundException("ERROR: Test Suite file not found at " + testSuiteFilePath);
+            }
+
+            var ruleApp = new FileSystemRuleApplicationReference(ruleAppFilePath);
+            RuleApplicationDef ruleAppDef = ruleApp.GetRuleApplicationDef();
+
+            TestSuiteDef suite = TestSuiteDef.LoadFrom(new ZipFileTestSuitePersistenceProvider(testSuiteFilePath));
+            suite.ActiveRuleApplicationDef = ruleAppDef;
+
+            TestResultCollection results;
+            using (TestingSessionManager manager = new TestingSessionManager(new InProcessConnectionFactory()))
+            {
+                var session = new RegressionTestingSession(manager, suite);
+                results = session.ExecuteAllTests();
+            }
+
+            return results;
+        }
+    }
+}
